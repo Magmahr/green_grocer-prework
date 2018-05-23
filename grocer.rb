029de26f3c2ca236b42fa62d65c94ebfc28a998f
Matt Maher <mattmaher0@gmail.com>
2018-05-23 14:42:36 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -29,7 +29,7 @@ def apply_coupons(cart, coupons)
   if cart.keys.include?(item)
     if cart[item][:count] >= coupon[:num]
       cart[item][:count] -= coupon[:num]
-      
+      cart["#{item} W/COUPON"] = coupon[:price]
 end
 
 def apply_clearance(cart)
